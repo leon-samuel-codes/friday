50b15a8f0c34859b6d0b903442b97d656b66dcad
Leon <leonsamuels241@gmail.com>
2026-09-15 17:42:47 +0530

    Added memory split

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
+
+
 from ollama import chat
-import json
 
+from memory import load_memory,  save_memory 
 
-def load_memory():
-    try:
-        with open("memory.json", "r") as file:
-            data = json.load(file)
-        if "facts" not in data:          
-            data["facts"] = []
-        return data
-    except FileNotFoundError:
-        return {"name": None, "facts": []}
-               
-def save_memory():
-    with open("memory.json", "w") as file:
-        json.dump(memory , file, indent=2)  
 memory = load_memory()
 
 
@@ -22,7 +11,7 @@ memory = load_memory()
 if memory.get("name") is None:              
     name = input("FRIDAY: I don't know you yet. What's your name? ")
     memory["name"] = name
-    save_memory()                           
+    save_memory(memory)                           
     print(f"FRIDAY: Nice to meet you, {name}!")
 else:
     print(f"FRIDAY: Welcome back, {memory['name']}!")
@@ -50,7 +39,7 @@ while True:
     if user_input.lower().startswith("remember "):
         fact=user_input[9:]
         memory["facts"].append(fact)
-        save_memory()
+        save_memory(memory)
         print(f"FRIDAY: Got it! I will remember {fact}")
     messages.append({"role": "user", "content": user_input})  
 
---
+++ memory.py
@@ -0,0 +1,14 @@
+import json
+def load_memory():
+    try:
+        with open("memory.json", "r") as file:
+            data = json.load(file)
+        if "facts" not in data:          
+            data["facts"] = []
+        return data
+    except FileNotFoundError:
+        return {"name": None, "facts": []}
+               
+def save_memory(memory):
+    with open("memory.json", "w") as file:
+        json.dump(memory , file, indent=2)  
